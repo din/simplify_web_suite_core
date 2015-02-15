1840570b95e56fc148618f76b3e697cf83ff7cc2
Maxim Melnikov <lion@mmth.us>
2015-02-15 21:36:27 +0300

    Fixed 8tracks misbehaviour
    Simplify controls 8tracks’ player properly now. Also, seeking and
    backward features are disabled now.

--- players/8tracks.js
+++ players/8tracks.js
@@ -1,121 +1,119 @@
-// 8tracks.com
-
-(function() {
-
-  var ET_DEBUG_LOG = false;
-  if (ET_DEBUG_LOG) {
-    // Restore the console variable for debugging since 8tracks kills it.
-    delete window.console;
-  }
-
-  var log = function() {
-    if (ET_DEBUG_LOG) console.debug(arguments);
-  };
-
-  var trax, mixPlayer, trackPlayer;
-
-  var updateSimplifyMetadata = function(simplify) {
-    var author, title, url, album, duration;
-    var track = App.Trax.mixPlayer && App.Trax.mixPlayer.track;
-    var mix = App.Trax.mix;
-    var trackPlayer = App.Trax.mixPlayer && App.Trax.mixPlayer.trackPlayer;
-
-    if (track) {
-      artist  = track.get('performer');
-      url = track.get('url');
-      title = track.get('name');
-      album = track.get('release_name');
-      duration =
-        trackPlayer.soundManagerPlayer.smSound.durationEstimate / 1000 || 9999;
-    } else if (mix) {
-      artist = mix.get('user').login;
-      url = 'http://8tracks.com' + mix.get('web_path');
-      title = mix.get('name');
-      album = mix.get('genres')[0];
-      duration = mix.get('duration') / 1000;
-    }
-    var trackInfo = {
-      author: artist || 'N/A',
-      title: title || 'N/A',
-      album: album || 'N/A',
-      length: duration || 'N/A',
-      uri: url || 'N/A'
-    };
-
-    log('Setting track info', trackInfo);
-    simplify.setCurrentTrack(trackInfo);
-    artworkUrl = mix.get('cover_urls').sq500;
-    simplify.setCurrentArtwork(artworkUrl);
-  };
-
-  var setupSimplifyBindings = function(simplify) {
-    // Requests
-    simplify.bindToVolumeRequest(function() {
-      return App.Trax.mixPlayer.trackPlayer.globalVolume;
-    }).bindToTrackPositionRequest(function() {
-      return App.Trax.mixPlayer.trackPlayer.currentPositionInMs / 1000;
-    });
-
-    // Actions by User
-    simplify.bind(Simplify.MESSAGE_DID_CHANGE_PLAYBACK_STATE, function(data) {
-      if (data.state == Simplify.PLAYBACK_STATE_PLAYING) {
-        App.Trax.play_page();
-      }
-      if (data.state == Simplify.PLAYBACK_STATE_PAUSED ||
-          data.state == Simplify.PLAYBACK_STATE_STOPPED) {
-        App.Trax.pausePlayback();
-      }
-    }).bind(Simplify.MESSAGE_DID_SELECT_PREVIOUS_TRACK, function() {
-      // 3000 ms is the lowest value 8tracks will accept.
-      App.Trax.mixPlayer.trackPlayer.seekTo(3000);
-    }).bind(Simplify.MESSAGE_DID_SELECT_NEXT_TRACK, function() {
-      App.Trax.mixPlayer.next();
-    }).bind(Simplify.MESSAGE_DID_CHANGE_VOLUME, function(data) {
-      App.Trax.mixPlayer.setVolume(data.amount);
-    }).bind(Simplify.MESSAGE_DID_CHANGE_TRACK_POSITION, function(data) {
-      App.Trax.mixPlayer.trackPlayer.seekTo(data.amount * 1000);
-    });
-  };
-
-  if (window.top == window) {
-    window.addEventListener('load', function() {
-      require(['global_trax'], function(trax) {
+if (window.top == window) {
+    window.addEventListener("load", function() {
         var simplify = new Simplify();
-        simplify.setCurrentPlayer('8tracks');
-
-        setupSimplifyBindings(simplify);
-        updateSimplifyMetadata(simplify);
-        // FIXME Start paused, this isn't working.
-        simplify.setNewPlaybackState(Simplify.PLAYBACK_STATE_PAUSED);
-
-        window.addEventListener('unload', function() {
-          simplify.closeCurrentPlayer();
-        });
+        simplify.setCurrentPlayer("8tracks");
 
         interval = setInterval(function() {
-          var mixPlayer = App.Trax.mixPlayer;
-          if (mixPlayer) {
-            clearInterval(interval);
-            mixPlayer.on('play', function() {
-              log('play');
-              simplify.setPlaybackPlaying();
-            });
-            mixPlayer.on('pause', function() {
-              log('paused');
-              updateSimplifyMetadata(simplify);
-              simplify.setPlaybackPaused();
-            });
-            mixPlayer.on('doneLoading', function() {
-              log('done loading');
-              updateSimplifyMetadata(simplify);
-            });
-            mixPlayer.trackPlayer.on('seconds:1', function() {
-              log('1 second');
-              updateSimplifyMetadata(simplify);
-            });
-          }
+            var mixPlayer = App.Trax.mixPlayer;
+            if (mixPlayer) {
+                clearInterval(interval);
+                mixPlayer.on('play', function() {
+                    simplify.setPlaybackPlaying();
+                });
+                mixPlayer.on('resume', function() {
+                    simplify.setPlaybackPlaying();
+                });
+                mixPlayer.on('pause', function() {
+                    updateSimplifyMetadata(simplify);
+                    simplify.setPlaybackPaused();
+                });
+                mixPlayer.on('doneLoading', function() {
+                    updateSimplifyMetadata(simplify);
+                });
+                mixPlayer.trackPlayer.on('seconds:1', function() {
+                    updateSimplifyMetadata(simplify);
+                });
+            }
         }, 500);
-      });
-    });
-  }
-})();
+
+        window.lastTrackId = null;
+        window.trackChanged = true;
+        updateSimplifyMetadata = function(simplify) {
+            var song = App.Trax.mixPlayer;
+            if (song.trackPlayer.soundManagerPlayer.smSound.loaded == false)
+            {
+              setTimeout(function()
+              {
+                updateSimplifyMetadata(simplify);
+              }, 500);
+              return;
+            }
+
+            if (window.lastTrackId != song.track.attributes["id"]) {
+                simplify.setCurrentTrack({
+                    "author": song.track.attributes["performer"],
+                    "title": song.track.attributes["name"],
+                    "album": song.track.attributes["release_name"],
+                    "length": parseInt(song.trackPlayer.soundManagerPlayer.smSound.durationEstimate / 1000),
+                    "features": {"disable_track_seeking" : true, "disable_previous_track" : true}
+                });
+        
+                simplify.setCurrentArtwork(App.Trax.mix.get('cover_urls').sq500);
+
+                window.lastTrackId = song.track.attributes["id"];
+            }
+
+            return song;
+        };
+
+        // var oldPause = App.Trax.mixPlayer.pause;
+        // App.Trax.mixPlayer.pause = function() {
+        //     var result = oldPause.apply(this);
+        //     updateSimplifyMetadata(simplify);
+        //     simplify.setPlaybackPaused();
+        //     return result;
+        // };
+
+        // var oldPlay = App.Trax.mixPlayer.play;
+        // App.Trax.mixPlayer.play = function() {
+        //     var result = oldPlay.apply(this);
+        //     updateSimplifyMetadata(simplify);
+        //     simplify.setPlaybackPlaying();
+        //     return result;
+        // };
+
+
+        simplify.bindToVolumeRequest(function() {
+            if (typeof App.Trax.mixPlayer == "undefined") return 0;
+            return App.Trax.mixPlayer.trackPlayer.globalVolume;
+
+        }).bindToTrackPositionRequest(function() {
+            if (typeof App.Trax.mixPlayer == "undefined") return 0;
+            if (window.trackChanged == false) {
+                return 0;
+            }
+            return parseInt(App.Trax.mixPlayer.trackPlayer.currentPositionInMs / 1000);
+        }).bind(Simplify.MESSAGE_DID_SELECT_PREVIOUS_TRACK, function() {
+            // Nothing to operate
+        }).bind(Simplify.MESSAGE_DID_SELECT_NEXT_TRACK, function() {
+            if (typeof App.Trax.mixPlayer == "undefined") return;
+            App.Trax.mixPlayer.next();
+            simplify.setPlaybackPlaying();
+            window.trackChanged = false;
+            setTimeout(function() {
+                window.trackChanged = true;
+            }, 300);
+        }).bind(Simplify.MESSAGE_DID_CHANGE_PLAYBACK_STATE, function(data) {
+
+            if (typeof App.Trax.mixPlayer == "undefined") return;
+            if (data["state"] == Simplify.PLAYBACK_STATE_PLAYING) {
+                App.Trax.mixPlayer.play();
+                return
+            };
+            if (data["state"] == Simplify.PLAYBACK_STATE_PAUSED) {
+                App.Trax.mixPlayer.pause();
+                return
+            };
+        }).bind(Simplify.MESSAGE_DID_CHANGE_VOLUME, function(data) {
+            if (data["amount"] == null || typeof App.Trax.mixPlayer == "undefined") return;
+            App.Trax.mixPlayer.setVolume(data.amount);
+        }).bind(Simplify.MESSAGE_DID_CHANGE_TRACK_POSITION, function(data) {
+            if (data["amount"] == null || typeof dzPlayer == "undefined") return;
+            App.Trax.mixPlayer.trackPlayer.seekTo(parseFloat(data["amount"]) * 1000);
+        });
+
+        window.addEventListener("unload", function() {
+            simplify.closeCurrentPlayer();
+        });
+    })
+}
