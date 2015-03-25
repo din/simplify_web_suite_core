dc57cd5e157c56bf92f7ce21d9dc1972b4a39cd3
Elan Feingold <elan@plex.tv>
2015-03-25 09:05:57 -1000

    Simple Plex integration.

---
+++ players/plex.js
@@ -0,0 +1,81 @@
+(function() {
+  function readSlider() {
+    if ($('.seek-bar-container').length && $('.player-slider-progress').length) {
+      var time = $('.seek-bar-container .player-position').text().split(':');
+      time = parseInt(time[0], 10)*60 + parseInt(time[1], 10);
+      
+      var duration = $('.seek-bar-container .player-duration').text().split(':');
+      duration = parseInt(duration[0], 10)*60 + parseInt(duration[1], 10);
+      
+      var position = parseFloat($('.player-slider-progress')[0].style.width);
+      position = duration*position/100.0;
+      
+      return { max: duration, min: 0, current: position };
+    } else {
+      return { max: 0, min: 0, current: 0 };
+    }
+  }
+  var updateSimplifyMetadata = function(simplify, checkLast) {
+    var artist = $('.media-title .grandparent-title').text();
+    var title = $('.media-title .item-title').text();
+    var playhead = readSlider();
+    
+    if (artist && title) {
+      simplify.setCurrentTrack({ 
+        author: artist, 
+        title: title, 
+        album: '', length: playhead.max, 
+        features: {
+          disable_track_seeking: true,
+          disable_previous_track: $('.previous-btn').hasClass('disabled'),
+          disable_next_track: $('.next-btn').hasClass('disabled')
+      }});
+    }
+
+    if ($('.mini-player').length) {
+      if ($('.mini-controls-left .play-btn').css('display') == 'none') {
+        simplify.setNewPlaybackState(Simplify.PLAYBACK_STATE_PLAYING);
+        //simplify.setCurrentArtwork(null);
+      } else {
+        simplify.setNewPlaybackState(Simplify.PLAYBACK_STATE_PAUSED);
+      }
+    } else {
+      simplify.setNewPlaybackState(Simplify.PLAYBACK_STATE_STOPPED);
+    }
+  };
+
+  if (window.top == window) {
+    window.addEventListener('load', function() {
+      var body = document.getElementsByTagName("body")[0];
+      var script = document.createElement('script');
+      script.type = "text/javascript";
+      script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js";
+      body.appendChild(script);
+      var simplify = new Simplify();
+      simplify.setCurrentPlayer('Plex');
+
+      setInterval(function() {
+        updateSimplifyMetadata(simplify, true);
+      }, 1000);
+
+      simplify.bindToTrackPositionRequest(function() {
+        var playhead = readSlider();
+        return playhead.current;
+
+      }).bind(Simplify.MESSAGE_DID_SELECT_NEXT_TRACK, function() {
+        $('.mini-controls-left .next-btn').click();
+      }).bind(Simplify.MESSAGE_DID_SELECT_PREVIOUS_TRACK, function() {
+        $('.mini-controls-left .previous-btn').click();
+      }).bind(Simplify.MESSAGE_DID_CHANGE_PLAYBACK_STATE, function() {
+        if ($('.mini-controls-left .play-btn').css('display') == 'none')
+          $('.pause-btn').click();
+        else
+          $('.mini-controls-left .play-btn').click();
+      })
+
+      window.addEventListener('unload', function() {
+        simplify.closeCurrentPlayer();
+      });
+    });
+  }
+})();
